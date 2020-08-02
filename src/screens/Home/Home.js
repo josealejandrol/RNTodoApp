2848d5ac913279411a2def48f2561ab77802ef98
Jose Alejandro <carmens_321@outlook.com>
2020-08-02 14:04:15 -0500

    Se instalaron iconos y se configuro en ios y android

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Home = () => {
   return (
     <SafeAreaView>
       <View>
         <Text style={{fontFamily: 'OpenSans-Bold'}}>Soy HomeScreen</Text>
+        <Icon name={'emoticon'} size={28} color={'#000'} />
       </View>
     </SafeAreaView>
   );
